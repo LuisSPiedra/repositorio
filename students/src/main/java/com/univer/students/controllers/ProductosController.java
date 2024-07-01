412973319ba26803df23d6a5f4a4f31b51bd6472
LuisSPiedra <157914903+LuisSPiedra@users.noreply.github.com>
2024-06-30 20:01:52 -0600

    Add files via upload

--- students/src/main/java/com/univer/students/controllers/ProductosController.java
+++ students/src/main/java/com/univer/students/controllers/ProductosController.java
@@ -1,18 +1,39 @@
 package com.univer.students.controllers;
 
+import com.univer.students.entity.ProductoEntity;
+import com.univer.students.model.request.ProductoRequest;
+import com.univer.students.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
-@RequestMapping
-
+@RequestMapping("/api/product")
 public class ProductosController {
-    @PostMapping
-    public ResponseEntity<String> save2(@RequestBody ProductoResquest resquest){
-        
+
+    @Autowired
+    private ProductoRepository repository;
+
+    @GetMapping("/producto/{id}")
+    public ProductoEntity productos(@PathVariable Integer id){
+        return this.repository.findById(id).get();
+        //return Arrays.asList("tornillos", "clavos","pegamento");
+
     }
 
+    @PostMapping("/save/product")
+    public ResponseEntity<String> save(@RequestBody ProductoRequest request){
+        ProductoEntity entity=new ProductoEntity();
+        entity.setNombre(request.getName());
+        entity.setSku(request.getSku());
+        entity.setCantidad(request.getCantidad());
+        this.repository.save(entity);
+        return ResponseEntity.ok("El registro de realizo de manera correcto");
+    }
+
+
 }
+
---
+++ students/src/main/java/com/univer/students/repository/ProductoRepository.java
@@ -0,0 +1,5 @@
+package com.univer.students.repository;
+
+@org.springframework.stereotype.Repository
+public interface ProductoRepository extends org.springframework.data.repository.CrudRepository<com.univer.students.entity.ProductoEntity,java.lang.Integer> {
+}
